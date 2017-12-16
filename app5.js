901d773e6f4ffae7217ab7dccc06e8ec7c713296
tuxito <tuxito@agente01.capitantux.com>
2017-12-16 12:58:48 -0500

    app5.js

---
+++ app5.js
@@ -0,0 +1,19 @@
+var http=require('http'),
+    fs=require('fs');
+
+    var file=fs.readFileSync('archivo.csv',function (err, data) {
+        if (err) throw err;
+        resp.write(data);
+        });
+
+    	http.createServer(function (req,res){
+
+         this.archivo=file.toString();
+         var arr=archivo.split("\n");
+         res.writeHead(200,{'Content-Type': 'application/json'});
+         res.write(JSON.stringify({arreglo:arr}));
+         res.end();
+
+    	}).listen(puerto);
+
+    	console.log("Escuchando en el puerto",puerto);
