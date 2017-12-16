7a1c542cc7606c6811980c41af60156d72b604f8
tuxito <tuxito@agente01.capitantux.com>
2017-12-16 13:07:26 -0500

    modificacion app5

--- app5.js
+++ app5.js
@@ -8,8 +8,8 @@ var http=require('http'),
 
     	http.createServer(function (req,res){
 
-         var archivo=file.toString();
-         var arr=archivo.split("\n");
+         var archivo =file.toString();
+         var arr = archivo.split("\n");
          res.writeHead(200,{'Content-Type': 'application/json'});
          res.write(JSON.stringify({arreglo:arr}));
          res.end();
