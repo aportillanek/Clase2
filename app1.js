60e9fa71226e65fc205ec76e7acd35801b4a1eb6
tuxito <tuxito@agente01.capitantux.com>
2017-12-16 11:19:43 -0500

    cambio nuevo app

--- app1.js
+++ app1.js
@@ -1,9 +1,30 @@
-var http=require('http');
-var puerto=3000;
-http.createServer(function(req,res){
-   res.writeHead(200);
-   res.write("Hola mundo") ;
-   res.end();
-}).listen(puerto);
+//var http=require('http');
+//var puerto=3000;
+//http.createServer(function(req,res){
+ //  res.writeHead(200);
+ //  res.write("Hola mundo") ;
+ //  res.end();
+//}).listen(puerto);
 
+//console.log("Estamos escuchando en el puerto ",puerto);
+
+
+var http =require('http');
+
+var puerto= 3000;
+
+var manejador =function (req,res)
+{
+  res.writeHead(200);
+ res.write("Hola mundo \n") ;
+  res.end();
+
+
+}
+
+
+var servidor= http.createServer(manejador);
+servidor.listen(puerto);
 console.log("Estamos escuchando en el puerto ",puerto);
+
+
